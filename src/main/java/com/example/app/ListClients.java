96e9c393f511902fb1b99c0b7802083463efc976
firas112 <Firas.Garci@esprit.tn>
2023-01-24 20:01:01 +0100

    listClient

---
+++ src/main/java/com/example/app/ListClients.java
@@ -0,0 +1,63 @@
+package com.example.app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ListClients {
+
+    @FXML
+    private Button chercherBtn;
+
+    @FXML
+    private TextField chercherField;
+
+    @FXML
+    private TableColumn<?, ?> creditsC;
+
+    @FXML
+    private TableView<?> listClient;
+
+    @FXML
+    private AnchorPane listClientPane;
+
+    @FXML
+    private TableColumn<?, ?> nomC;
+
+    @FXML
+    private Button nouveauClientBtn;
+
+    @FXML
+    private TableColumn<?, ?> numTelC;
+
+    @FXML
+    private Button sedeconnecterBtn;
+
+    @FXML
+    private Button voirClientBtn;
+
+    @FXML
+    void chercherClient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chercherClientField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nouveauClient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voirClient(ActionEvent event) {
+
+    }
+
+}
