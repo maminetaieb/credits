a5232c0478f8d06c4e58f23ead7e3273ecafa7d0
firas112 <Firas.Garci@esprit.tn>
2023-01-25 00:35:33 +0100

    next

---
+++ src/main/java/com/example/app/LIstCredits.java
@@ -0,0 +1,46 @@
+package com.example.app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class LIstCredits {
+
+    @FXML
+    private Text CreditsText;
+
+    @FXML
+    private AnchorPane ListCreditsPane;
+
+    @FXML
+    private Text MontantMaxText;
+
+    @FXML
+    private Button NouveaCreditBtn;
+
+    @FXML
+    private Button SupprimerClientBtn;
+
+    @FXML
+    private ListView<?> listCreditView;
+
+    @FXML
+    private Text nomText;
+
+    @FXML
+    private Text numtelText;
+
+    @FXML
+    void nouveauCredit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void supprimerClient(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/app/ModifMaxController.java
@@ -0,0 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField ModifMax;
+
+    @FXML
+    private Button ValiderBtn;
+
+    @FXML
+    private AnchorPane moidefMaxPane;
+
+    @FXML
+    void modifMax(ActionEvent event) {
+
+    }
+
+    @FXML
+    void valider(ActionEvent event) {
+
+    }
+
+}
