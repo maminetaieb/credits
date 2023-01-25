76eaba6cdcfc3908b02a8fceb56fef2466710f69
firas112 <Firas.Garci@esprit.tn>
2023-01-25 21:42:35 +0100

    lllll

---
+++ src/main/java/com/example/app/Login.java
@@ -0,0 +1,29 @@
+package com.example.app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Login {
+
+    @FXML
+    private Button LoginBtn;
+
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
