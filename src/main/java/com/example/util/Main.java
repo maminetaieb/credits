d2f592e0b8d67f26cbb902a01e4f12b3472d9fd7
mat <mamineteyeb@outlook.com>
2023-01-25 23:11:11 +0100

    dsf

--- src/main/java/com/example/util/Main.java
+++ src/main/java/com/example/util/Main.java
@@ -3,9 +3,11 @@ package com.example.util;
 import com.example.app.HelloApplication;
 import com.example.service.ClientService;
 import com.example.service.CreditService;
+import com.example.service.UserService;
 
 public class Main {
     public static void main(String[] args) {
+        new UserService().initialize();
         new ClientService().initialize();
         new CreditService().initialize();
         HelloApplication.main(args);
