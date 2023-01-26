f4d66617eca324469ffae3e813501eaeb6f562d9
mat <mamineteyeb@outlook.com>
2023-01-27 00:12:42 +0100

    kkdfk

--- src/main/java/com/example/app/AdminPage.java
+++ src/main/java/com/example/app/AdminPage.java
@@ -1,16 +1,19 @@
 package com.example.app;
 
+import com.example.entity.Credit;
 import com.example.entity.User;
+import com.example.service.CreditService;
 import com.example.service.UserService;
+import com.example.util.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
 
 public class AdminPage {
 
@@ -41,7 +44,25 @@ public class AdminPage {
 
     @FXML
     void deleteuser(ActionEvent event) {
-
+        try {
+            User u = new User(nameField.getText(), passField.getText());
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Supprimer utilisateur");
+            alert.setContentText("Voulez vous vraiment supprimer cet utilisateur?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                try {
+                    new UserService().delete(u);
+                } catch (Exception e) {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Utilisateur non existant");
+                    alert.setContentText("Veuillez entrer un login valide");
+                    alert.show();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
