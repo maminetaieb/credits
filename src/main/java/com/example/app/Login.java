f32a219ecc3d99dab6b102e2fec24f81a7711550
firas112 <Firas.Garci@esprit.tn>
2023-01-27 01:00:38 +0100

    last

--- src/main/java/com/example/app/Login.java
+++ src/main/java/com/example/app/Login.java
@@ -4,6 +4,7 @@ import com.example.service.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -30,7 +31,8 @@ public class Login {
     void login(ActionEvent event) throws IOException {
         if (nameField.getText().equals("admin") && passField.getText().equals("F@goçytoSe")) {
             AnchorPane pane = FXMLLoader.load(getClass().getResource("AdminPage.fxml"));
-            loginPane.getChildren().setAll(pane);
+
+ 
         } else if (new UserService().authenticate(nameField.getText(), passField.getText()) != null) {
             AnchorPane pane = FXMLLoader.load(getClass().getResource("ListClients.fxml"));
             loginPane.getChildren().setAll(pane);
